2e0843566747971b377713742626bafc081c8105
night-fury-3 <jonathonfrakes3@gmail.com>
2024-08-14 22:19:29 -0400

    2. Core Concepts the building blocks of Rust
    fix: Splitting a Vector

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,18 @@ impl Deck {
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
 }
 
 fn main() {
     let mut deck = Deck::new();
     deck.shuffle();
+    // Probably need to add error handling!!!!
+    let cards = deck.deal(3);
 
+    println!("Heres your hand: {:#?}", cards);
     println!("Heres your deck: {:#?}", deck);
 }
